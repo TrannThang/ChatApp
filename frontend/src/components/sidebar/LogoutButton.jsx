868f5c70376b56401dd444083fcf4ba7da50f08f
TrannThang <trannthang2212@gmail.com>
2024-06-27 19:02:18 +0700

    First commit

---
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -0,0 +1,11 @@
+import { IoMdLogOut } from "react-icons/io";
+
+const LogoutButton = () => {
+  return (
+    <div className="mt-auto">
+      <IoMdLogOut className="w-6 h-6 text-white cursor-pointer" />
+    </div>
+  );
+};
+
+export default LogoutButton;
